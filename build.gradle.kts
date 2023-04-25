5f8c3435d2163d0d5068d33d4feda632f63f49d4
Etienne Vautherin <etienne@vautherin.com>
2023-04-25 20:31:44 +0200

    nativecoroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.android.library").version("8.1.0-beta01").apply(false)
     kotlin("android").version("1.8.10").apply(false)
     kotlin("multiplatform").version("1.8.10").apply(false)
+    id("com.rickclephas.kmp.nativecoroutines").version("1.0.0-ALPHA-5").apply(false)
+
 }
 
 tasks.register("clean", Delete::class) {
